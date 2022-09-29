11988b4fa42e06c4cb2208bce8970b8a8dfec885
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-09-30 00:23:33 +0300

    Теперь точно правильные файлы

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        long[] sales = new long[]{10000L, 20000L, 60000L, 100000L};
+        long[] sales = {10000L, 20000L, 60000L, 100000L};
         SalesManager salesMan = new SalesManager(sales);
         System.out.println("Расчет среднего: " + salesMan.statistik());
     }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,45 +1,37 @@
 import java.util.stream.LongStream;
 
-    public class SalesManager {
-        protected long[] sales;
+public class SalesManager {
+    protected long[] sales;
 
-        public SalesManager(long[] sales) {
-            this.sales = sales;
-        }
-
-        public long max() {
-            long max = -1L;
-            long[] var3 = this.sales;
-            int var4 = var3.length;
-
-            for(int var5 = 0; var5 < var4; ++var5) {
-                long sale = var3[var5];
-                if (sale > max) {
-                    max = sale;
-                }
-            }
-
-            return max;
-        }
-
-        public long min() {
-            long min = 100000L;
-            long[] var3 = this.sales;
-            int var4 = var3.length;
-
-            for(int var5 = 0; var5 < var4; ++var5) {
-                long sale = var3[var5];
-                if (sale < min) {
-                    min = sale;
-                }
-            }
-
-            return min;
-        }
-
-        public long statistik() {
-            long statistik = (LongStream.of(this.sales).sum() - this.max() - this.min()) / (long)(this.sales.length - 2);
-            return statistik;
-        }
+    public SalesManager(long[] sales) {
+        this.sales = sales;
     }
+
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+
+        return max;
+    }
+
+    public long min() {
+        long min = 100_000;
+        for (long sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+
+        return min;
+    }
+
+    public long statistik() {
+        long statistik = (LongStream.of(sales).sum() - max() - min()) / (sales.length - 2);
+        return statistik;
+    }
+}
 
