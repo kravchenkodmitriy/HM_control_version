9216270c82c7fa95a3f2ec23d6b667b498420281
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-09-30 00:06:19 +0300

    Добавление файлов

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        long[] sales = {10_000, 20_000, 60_000, 100_000};
+        SalesManager salesMan = new SalesManager(sales);
+        System.out.println("Расчет среднего: " + salesMan.statistik());
+    }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,34 @@
-public class SalesManager {
-    protected int[] sales;
+import java.util.stream.LongStream;
 
-    public SalesManager(int[] sales) {
+public class SalesManager {
+    protected long[] sales;
+
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-            for (int sale : sales){
+    public long max() {
+        long max = -1;
+            for (long sale : sales){
                 if (sale > max){
                     max = sale;
                 }
             }
         return max;
+    }
+
+    public long min() {
+        long min = 100_000;
+            for (long sale : sales){
+                if (sale < min){
+                    min = sale;
+                }
+            }
+            return min;
+    }
+
+    public long statistik(){
+        long statistik = (LongStream.of(sales).sum() - max() - min()) / (sales.length - 2);
+        return statistik;
     }
 }
